8525ec4789527127c79b091ffc27b4eae44f5caf
banch0u <fikret.okis@gmail.com>
2025-02-17 21:15:24 +0400

    fileView added

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@
 import "./assets/fonts/fonts.css";
 export { default as Button } from "./components/Button";
 export { default as ColSort } from "./components/ColSort";
+export { default as FileView } from "./components/FileView";
