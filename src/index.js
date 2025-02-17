327ee19cb6879453abe200e78ae3f89f864bfb60
banch0u <fikret.okis@gmail.com>
2025-02-17 20:46:10 +0400

    typo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 // Export the Button component from the components directory as a named export
 import "./assets/fonts/fonts.css";
 export { default as Button } from "./components/Button";
-export { default as ColSort } from "./components/Button";
+export { default as ColSort } from "./components/ColSort";
