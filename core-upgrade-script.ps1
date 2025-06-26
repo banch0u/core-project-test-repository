544eba26cd2ffe5bfc6ef0db98f7318a5331f0e3
banch0u <fikret.okis@gmail.com>
2025-06-26 15:05:36 +0400

    core upgrade script added

---
+++ core-upgrade-script.ps1
@@ -0,0 +1,21 @@
+$folders = @(
+  "C:\Users\fikre\repos\account\frontend",
+  "C:\Users\fikre\repos\archive\frontend",
+  "C:\Users\fikre\repos\contract\frontend",
+  "C:\Users\fikre\repos\docflow\frontend",
+  "C:\Users\fikre\repos\hr\frontend",
+  "C:\Users\fikre\repos\lab\frontend",
+  "C:\Users\fikre\repos\transport-frontend"
+)
+
+foreach ($folder in $folders) {
+  Write-Host "`nProcessing: $folder" -ForegroundColor Cyan
+  Set-Location $folder
+
+  git checkout dev
+  git pull
+  npm i @banch0u/core-project-test-repository
+  git add .
+  git commit -m "pkg upgrade"
+  git push
+}
