f50a3b76217855c59c389a859b86ffde54f1299a
banch0u <fikret.okis@gmail.com>
2026-09-14 12:13:58 +0400

    vendorcategory added

---
+++ src/components/VendorCategoryTreeSelect/index.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { TreeSelect } from "antd";
+import { useDispatch, useSelector } from "react-redux";
+import { getVendorCategoriesAll } from "../../store/slices/questionnaire";
+import style from "./index.module.scss";
+
+const mapCategoriesToTree = (nodes = []) =>
+  nodes.map((node) => ({
+    title: node.name,
+    value: node.id,
+    children: node.children?.length ? mapCategoriesToTree(node.children) : [],
+  }));
+
+const VendorCategoryTreeSelect = ({ value, onChange, ...rest }) => {
+  const dispatch = useDispatch();
+
+  const vendorCategoriesAll = useSelector(
+    (state) => state.questionnaire.vendorCategoriesAll,
+  );
+
+  useEffect(() => {
+    if (!vendorCategoriesAll?.length) {
+      dispatch(getVendorCategoriesAll("onlyactive"));
+    }
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  return (
+    <TreeSelect
+      value={value}
+      onChange={onChange}
+      treeData={mapCategoriesToTree(vendorCategoriesAll ?? [])}
+      showSearch
+      allowClear
+      treeNodeFilterProp="title"
+      placeholder=""
+      style={{ width: "100%", height: 42 }}
+      className={style.treeSelect}
+      {...rest}
+    />
+  );
+};
+
+export default VendorCategoryTreeSelect;
